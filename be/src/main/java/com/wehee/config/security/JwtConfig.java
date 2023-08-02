59c94a83f8cf3b405c5c8500df1bc64c161a3cd9
Yejining <kimyejin.kr@gmail.com>
2023-08-02 14:51:22 +0900

    S09P12A806-256/feat: add dependency for oauth, and add config files
    Former-commit-id: 6014a10b29a4bc799e1af2753d00e3556ec32bbb

---
+++ be/src/main/java/com/wehee/config/security/JwtConfig.java
@@ -0,0 +1,12 @@
+package com.wehee.config.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+}
---
+++ be/src/main/java/com/wehee/config/security/SecurityConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:28408d9c3e51c42ac2ea12df849c63f2fd581cf38b186bcbd81bf7d1ed3699bf
+size 1704

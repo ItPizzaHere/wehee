c6e79044b899e96042ffe81c29ba50e59b4b788f
박우현 <parkstarboard@gmail.com>
2023-08-03 18:01:38 +0900

    S09P12A806-148/feat: lounge CRUD
    Former-commit-id: c88b26b4db540f4d00836744627779e518ab1719

--- be/src/main/java/com/wehee/domain/lounge/service/LoungeService.java
+++ be/src/main/java/com/wehee/domain/lounge/service/LoungeService.java
@@ -1,4 +1,3 @@
-package com.wehee.domain.lounge.service;
-
-public class LoungeService {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac6c2d78ff3f4bf8823e36f978fd36a9c4023afa10f9cef69eead0445d32e6bf
+size 874
--- be/src/main/resources/schema.sql
+++ be/src/main/resources/schema.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cad9d20c1c1651ffd9de6693b2c0f8087359901619ff7375b9cb97836d98f33a
-size 7290
+oid sha256:f46b8b7f79763f98716205bf8b7f083854e1d69b5932588def259536265ff4fe
+size 7771

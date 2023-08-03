038cc65a224673462a42b930f215b13017e78bfa
Yejining <kimyejin.kr@gmail.com>
2023-08-03 20:36:29 +0900

    feat: update NaverOAuth2UserInfo class
    Former-commit-id: 0f231697dc5b68e5bd2511fa7e6e68b43566e994

--- be/src/main/java/com/wehee/domain/auth/info/impl/NaverOAuth2UserInfo.java
+++ be/src/main/java/com/wehee/domain/auth/info/impl/NaverOAuth2UserInfo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87eb7e03440a606fbf9dc68441393a28de6927c6db5036f8ef2349ac75c0e02e
-size 686
+oid sha256:748deceb85cf98fd968a6e15fe033cc78d5e80e8d064a08282f468db8dbc3a7f
+size 1538

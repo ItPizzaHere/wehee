684bae8e583a5fdd544cbf7c54f57d991b59f1c6
yeonuy <jyeonuy@gmail.com>
2023-08-04 01:51:48 +0900

    S09P12A806-228/refactor: 레이아웃 분리해 적용
    Former-commit-id: dc0796864c760e360fee78ed6791ad1463f505cc

--- fe/src/pages/UserLogin.tsx
+++ fe/src/pages/UserLogin.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b0bbac97fa6ddd2fb786ba9e0e83b5503adc9f0d3e75241202031213b5cd7b3
-size 926
+oid sha256:f1c72fae122f1215c33d24e8287b7211e82461c4fead21a965c2c937123c0d33
+size 917

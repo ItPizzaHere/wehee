d6bfc08ac74e7be4d392c2a5bc06d55311f29bb8
Yejining <kimyejin.kr@gmail.com>
2023-08-03 11:40:23 +0900

    S09P12A806-257/feat: modify User entity
    Former-commit-id: 0a904672514a8585200134ddbaef76e29276f7a7

--- be/src/main/java/com/wehee/domain/user/entity/User.java
+++ be/src/main/java/com/wehee/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f782d93b698b26866476d15a41ec273a7aa7ccd01b87c5ff18aa32522af535b
-size 1070
+oid sha256:de27d02d91fa99423ff97ba08abb121d9c4b86f20893930719b079ad9ca40bef
+size 1121

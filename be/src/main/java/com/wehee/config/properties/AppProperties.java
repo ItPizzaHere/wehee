53919c181321b9f73d5d3bf4e424c32b5ec028e0
Yejining <kimyejin.kr@gmail.com>
2023-08-04 00:49:11 +0900

    S09P12A806-260/feat: property classes for spring security
    Former-commit-id: e56dc3739045d889867b0ae3caa15068d84112ed

--- be/src/main/java/com/wehee/config/properties/AppProperties.java
+++ be/src/main/java/com/wehee/config/properties/AppProperties.java
@@ -1,4 +1,3 @@
-package com.wehee.config.properties;
-
-public class AppProperties {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1f51266e54373b6df987f2390864bb9ef19bfb4615da043440d34341a8e1b054
+size 1046

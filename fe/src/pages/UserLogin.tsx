a8b3c294f5aa881306ae5ed302be875c132b891a
yeonuy <jyeonuy@gmail.com>
2023-08-03 12:36:05 +0900

    S09P12A806-228/feat: 로그인창에 배경 삽입
    Former-commit-id: f509b645c717f6f4c6bf0ca29c5b2496a4418fed

--- fe/src/pages/UserLogin.tsx
+++ fe/src/pages/UserLogin.tsx
@@ -1,14 +1,3 @@
-import * as React from 'react';
-import HeaderBeforeLogin from 'components/header/HeaderBeforeLogin';
-import SocialLoginWindow from 'components/user/SocialLoginWindow';
-
-function UserLogin() {
-  return (
-    <div>
-      <HeaderBeforeLogin hide={true} />
-      <SocialLoginWindow />
-    </div>
-  );
-}
-
-export default UserLogin;
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9086ab74d040cbb59a017e93c3f6cfa33357b617f9704026e81ad91cfef9882
+size 716

90a16625d6c508ff5fd6c5466ba9bdd04d378d28
Yejining <kimyejin.kr@gmail.com>
2023-08-07 17:55:19 +0900

    S09P12A806-283/feat: get loadUser() from UserController class
    Former-commit-id: a233974be053efabd207cd791aaf55f570719884

--- be/src/main/java/com/wehee/api/auth/AuthController.java
+++ be/src/main/java/com/wehee/api/auth/AuthController.java
@@ -1,4 +1,3 @@
-package com.wehee.api.auth;
-
-public class AuthController {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fd777d461a6940ac5b17961fa69ef741bfa894acafd2b92013b2cf7948b7f1eb
+size 1038
--- be/src/main/java/com/wehee/domain/user/service/UserService.java
+++ be/src/main/java/com/wehee/domain/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec91ba02d844332006a9fe4e3ad905ba1ff1dac4dae681acdd517ccba952dcd2
-size 2470
+oid sha256:995e579dc121db2098fd9c1cb3ca4dc797e820ed300acf52966c8dcc27065799
+size 2601

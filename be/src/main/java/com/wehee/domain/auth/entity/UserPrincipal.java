99cbd63227093e6e533d34f046baaadf6ac8d7c9
Yejining <kimyejin.kr@gmail.com>
2023-08-07 09:03:23 +0900

    S09P12A806-264/feat: create user in CustomOAuth2UserService
    Former-commit-id: d925a5b399baed70ffca3f00bc081285ff214820

--- be/src/main/java/com/wehee/domain/auth/entity/UserPrincipal.java
+++ be/src/main/java/com/wehee/domain/auth/entity/UserPrincipal.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:061e7e8ea9ff2e637e78e9853717948e83f8db33a77227e50e5575ed8c5da6ec
-size 1950
+oid sha256:e8f94f2d32df272a93eaa5d201f7cc2b6440cd4e7c831400435198926d404af1
+size 2471
--- be/src/main/java/com/wehee/domain/auth/info/impl/NaverOAuth2UserInfo.java
+++ be/src/main/java/com/wehee/domain/auth/info/impl/NaverOAuth2UserInfo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:748deceb85cf98fd968a6e15fe033cc78d5e80e8d064a08282f468db8dbc3a7f
-size 1538
+oid sha256:437207c51e105dbde49d036d2af97290547502b29d2914bdf69153d1a4eb5f26
+size 1643

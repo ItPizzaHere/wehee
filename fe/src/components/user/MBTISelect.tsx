ea6334fd701eca028b3c45c6fa2b9fd0156cfa8f
yeonuy <jyeonuy@gmail.com>
2023-08-07 11:39:28 +0900

    S09P12A806-277/feat: 최초 입력창 UI 완성 (제출 버튼은 추후 추가)
    Former-commit-id: 26acb6669a149be98ccbab8b996246962499112c

--- fe/src/components/user/MBTISelect.tsx
+++ fe/src/components/user/MBTISelect.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaff354a5115d4cf6585f51fbac030c1987589b75d68f6da6cfdf86ff1019f87
-size 2464
+oid sha256:5d0bddee55a35f13251a2794b6ac9063de4876800dabf00625cf4d2b6263bdc0
+size 2465

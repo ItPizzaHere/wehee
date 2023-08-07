8b5dac5cc9d27163cb68179d27f10bd9383bff83
Yejining <kimyejin.kr@gmail.com>
2023-08-07 16:10:19 +0900

    fix: fix grammatical errors
    Former-commit-id: eebfacaa18c06a89e1ac46718f4b3a62e09f46a3

--- be/src/main/java/com/wehee/config/security/SecurityConfig.java
+++ be/src/main/java/com/wehee/config/security/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f317273a7fb49bb66f472d0572409487ac76b8a9a33447f825257616a5fcf19a
-size 4609
+oid sha256:d58ff9d3272557065fc63e750fc67086e5ce50c69f31af7c4c292384019aea7e
+size 4610

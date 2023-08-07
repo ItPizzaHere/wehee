1edc30c6ed2ca0fdc4c4b55e55ca0f7971d64d33
Yejining <kimyejin.kr@gmail.com>
2023-08-07 16:10:19 +0900

    fix: fix grammatical errors
    Former-commit-id: 4d3b13ee777c9f2331a0aef5576a7e57b625be74

--- be/src/main/java/com/wehee/api/user/UserController.java
+++ be/src/main/java/com/wehee/api/user/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80cac6de230410ad53789c5c8148c59ac6bfa201f7ee4b40bfc91ad643d16026
-size 2020
+oid sha256:a1166d5d1de1c4f03fa14fdb72ec58d086827b2f40e430df70581c4b52679297
+size 1971

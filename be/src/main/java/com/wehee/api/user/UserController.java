c6ebccff8b30829f6573c7795e4a241481fe9bd1
Yejining <kimyejin.kr@gmail.com>
2023-08-07 17:55:19 +0900

    S09P12A806-283/feat: get loadUser() from UserController class
    Former-commit-id: a6a37c87dea24fd65f1cc11ea364eb86b5fa0e06

--- be/src/main/java/com/wehee/api/user/UserController.java
+++ be/src/main/java/com/wehee/api/user/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1166d5d1de1c4f03fa14fdb72ec58d086827b2f40e430df70581c4b52679297
-size 1971
+oid sha256:6f9bc083eba56b795d78441255ecab9e5f08de1efd106afba960e7c4e4a72233
+size 1794
--- be/src/main/java/com/wehee/domain/user/repository/UserRepository.java
+++ be/src/main/java/com/wehee/domain/user/repository/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByProviderId(String providerId);
     User findByNickname(String nickname);
 }

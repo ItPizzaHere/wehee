58225a04a6d7645d7bd51bb57b1fbf5083ef4fc9
박우현 <parkstarboard@gmail.com>
2023-08-07 17:39:43 +0900

    S09P12A806-148/feat: update table name(like -> `like`, comment -> `comment`), remove vote, reply, declare functions
    TODO AT HOME: RefreshToken.java, RefreshTokenRepository.java comment all
    add
      autoconfigure:
        exclude: org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
    at application.yml,
    add
                .csrf(c -> c.disable())
    at SecurityConfig.java
    for test
    
    
    Former-commit-id: e68b81c977bc3e426fff919bf6896e5907e5026f

--- be/src/main/java/com/wehee/domain/lounge/Reply.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2c5a4537b6b24d77ea9b35d49226cbcfef2a4d1b8fb1218b411f6b41dfdfba10
-size 1352
--- be/src/main/java/com/wehee/domain/lounge/Vote.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ffce745aa281eebfb8212b31bfd10695fb878cbc76fc88e411b7f2b7079d48e4
-size 1319

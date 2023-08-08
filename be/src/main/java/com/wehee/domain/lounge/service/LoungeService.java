67a619b0643e337e2eaa0c855550eeb027fada17
Yejining <kimyejin.kr@gmail.com>
2023-08-08 15:31:40 +0900

    fix: fix grammatical errors
    Former-commit-id: 45472130746efd5a1b5ef8ddbe6044a84d6ec91c

--- be/src/main/java/com/wehee/domain/lounge/service/LoungeService.java
+++ be/src/main/java/com/wehee/domain/lounge/service/LoungeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:635f93aeee9dd82d3dbab6d6ebe0d4bc2c1a47e230e753247b123a4ac460df32
-size 5855
+oid sha256:2982a302019829766eeed5e12114680356bc82e75fc8a26ddfd801d5fa328f30
+size 5757

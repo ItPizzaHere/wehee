5a657b10363765f555b0502d33650bc4032e2523
박우현 <parkstarboard@gmail.com>
2023-08-08 13:21:34 +0900

    S09P12A806-148/feat: implement comment(update comment), not done
    Former-commit-id: cc7ce78261657906d331d90982e0157e0d51c8e0

--- be/src/main/java/com/wehee/domain/lounge/repository/CommentRepository.java
+++ be/src/main/java/com/wehee/domain/lounge/repository/CommentRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c737bc9d2a33afad1f761520e1578b0578cbc01c9abb110e99e28462f14c938e
-size 1080
+oid sha256:1958f0dcd6a4f7662bc06f727f659a4040d00b84a66c935e26e25f4f39df794c
+size 1168

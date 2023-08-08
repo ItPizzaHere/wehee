3c852f98ee3da0a7082b5c896bb46b04d4fa9310
yeonuy <jyeonuy@gmail.com>
2023-08-08 15:07:40 +0900

    S09P12A806-262/fix: 미사용 Slice 삭제
    Former-commit-id: 3cf3758e9ef79d30bb1ba5e88c7265d072eebc3c

--- fe/src/redux/store.ts
+++ fe/src/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authRedcer from './authSlice';
 import userReducer from './userSlice'
 import mbtiReducer from './mbtiSlice'
 
 
 const store = configureStore({
   reducer: {
-    auth: authRedcer,
     user: userReducer,
     mbti: mbtiReducer,
   },

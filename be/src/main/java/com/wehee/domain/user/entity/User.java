11b75c776a55b24639f0d47df83f22853b5b939f
Yejining <kimyejin.kr@gmail.com>
2023-08-09 14:07:17 +0900

    S09P12A806-306/refactor: user my page
    Former-commit-id: 0192423623e92e8940c2b9a65064a9f09237ad67

--- be/src/main/java/com/wehee/domain/user/entity/User.java
+++ be/src/main/java/com/wehee/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a374dbebace1fae8b9f2d2f3135ac2e4ecd3bced1a7a57181dc3ae29615d0f6
-size 4217
+oid sha256:d1ed2e42331081d48b88402e233b46cc1dd6ce48c751ce9e7dcd25208c63819f
+size 3879

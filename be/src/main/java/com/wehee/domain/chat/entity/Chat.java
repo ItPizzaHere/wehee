ca137d979f2f27257972b6974a70659dc34b1872
Yejining <kimyejin.kr@gmail.com>
2023-08-09 10:07:53 +0900

    S09P12A806-189/feat: init classes for chatting
    Former-commit-id: 33bb7396ef4005587d954edfe917817f31498f06

---
+++ be/src/main/java/com/wehee/domain/chat/entity/Chat.java
@@ -0,0 +1,5 @@
+package com.wehee.domain.chat.entity;
+
+public class Chat {
+
+}
---
+++ be/src/main/java/com/wehee/domain/chat/repository/ChatRepository.java
@@ -0,0 +1,10 @@
+package com.wehee.domain.chat.repository;
+
+import com.wehee.domain.chat.entity.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+
+}

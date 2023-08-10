9a1356584a0d0da5cfaaa187a0a192bb0c3b5a72
Yejining <kimyejin.kr@gmail.com>
2023-08-10 15:22:13 +0900

    S09P12A806-312/feat: segregate renewRefreshToken() from refershToken() method in AuthController
    Former-commit-id: 12ff06eaf65966308841f87c85c1a9e178700d34

--- be/src/main/java/com/wehee/api/auth/AuthController.java
+++ be/src/main/java/com/wehee/api/auth/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad0ddefef4e250c577ac6b7aaf69f3e4c9464a6a1cfa49632d50385d7a4173b5
-size 3518
+oid sha256:bb5c397601a54b8251b32346f66408b8651568edae6a5d3d9e94fd26460e5168
+size 3412

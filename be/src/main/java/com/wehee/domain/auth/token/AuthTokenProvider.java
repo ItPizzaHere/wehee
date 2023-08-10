c50cb3ed479cec3dad77d74f198acdacfb3a46f6
Yejining <kimyejin.kr@gmail.com>
2023-08-10 13:05:24 +0900

    S09P12A806-312/feat: change LocalDateTime type to Date type in AuthToken related parts
    Former-commit-id: a873b9e53d777c05ff5f5284019e40d46591a041

--- be/src/main/java/com/wehee/domain/auth/token/AuthTokenProvider.java
+++ be/src/main/java/com/wehee/domain/auth/token/AuthTokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11f405f03f2a1a8ba6e99e8fe2cc31bc81aee34c6cfe77d8bc49637f551bae68
-size 2071
+oid sha256:9551bf67ed4d50d4637587759d40a869644a363eeaa28237caef3f439a091a4b
+size 2076
--- be/src/main/java/com/wehee/utils/TimeUtil.java
+++ be/src/main/java/com/wehee/utils/TimeUtil.java
@@ -3,13 +3,12 @@ package com.wehee.utils;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.TimeZone;
 
 public class TimeUtil {
 
-    public static LocalDateTime calExpiry(LocalDateTime time, long tokenExpiry) {
-        long expiry = Timestamp.valueOf(time).getTime() + tokenExpiry;
-        return LocalDateTime.ofInstant(
-            Instant.ofEpochMilli(expiry), TimeZone.getDefault().toZoneId());
+    public static long calExpiry(Date time, long tokenExpiry) {
+        return time.getTime() + tokenExpiry;
     }
 }

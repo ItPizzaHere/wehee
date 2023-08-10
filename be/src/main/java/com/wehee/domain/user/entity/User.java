8f323385c377b1f17663979230b17a4f426676c8
박우현 <0x847769a@gmail.com>
2023-08-10 17:12:37 +0900

    Update User.java
    Former-commit-id: 96d4c1472da0ee097f28114e739d5bc606b1a671

--- be/src/main/java/com/wehee/domain/user/entity/User.java
+++ be/src/main/java/com/wehee/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fb9dcfb7bf585bb1f61e0115256e9ba1219fafa24a6a1dfb52a667677b24e04
-size 3879
+oid sha256:6526a7c25501364d4fceda31f8f9c17ed2fe8fb25d22aeb6b4ae0c05cf8a0910
+size 3881

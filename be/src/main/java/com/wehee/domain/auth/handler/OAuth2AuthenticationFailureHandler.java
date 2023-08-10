faac9650b746e2ac7bd23e2934f0a0f1a1f46e89
Yejining <kimyejin.kr@gmail.com>
2023-08-10 14:44:58 +0900

    S09P12A806-312/feat: add cookie renew method in CookieUtil class
    Former-commit-id: 78130ec0d401910ea37ac9f5e46f03ab114d7e50

--- be/src/main/java/com/wehee/domain/auth/handler/OAuth2AuthenticationFailureHandler.java
+++ be/src/main/java/com/wehee/domain/auth/handler/OAuth2AuthenticationFailureHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:275dc944436bd8295f96fc44ae1c837fd91d7c5cefe89df2d518e4fee9a702db
-size 1693
+oid sha256:aeb8034ba61119119cd44fae126f02758a237ea5f41344098b2854acf90e4c8e
+size 1687
--- be/src/main/java/com/wehee/domain/auth/handler/OAuth2AuthenticationSuccessHandler.java
+++ be/src/main/java/com/wehee/domain/auth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adf7976d7f556fee0e3d7abf975302d4d918510fab3c164e15aa50a4cfed9f94
-size 5937
+oid sha256:1197688a3e76b405269b07b20de729eda52e04ab84a30b5a0b8fbe23ab65d930
+size 5859

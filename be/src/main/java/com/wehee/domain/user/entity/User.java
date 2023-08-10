282de2a2d817079e48aaf0a0e0a8bbcbc41a99c7
Yejining <kimyejin.kr@gmail.com>
2023-08-10 12:39:30 +0900

    feat: change table name user to user
    Former-commit-id: 42421c0a80f44133a3c33c6b79e897e3fc06e984

--- be/src/main/java/com/wehee/domain/user/entity/User.java
+++ be/src/main/java/com/wehee/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1ed2e42331081d48b88402e233b46cc1dd6ce48c751ce9e7dcd25208c63819f
+oid sha256:4fb9dcfb7bf585bb1f61e0115256e9ba1219fafa24a6a1dfb52a667677b24e04
 size 3879

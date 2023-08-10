a34fa1a7cb3c4a6934327a3b800760b49686beb4
Yejining <kimyejin.kr@gmail.com>
2023-08-10 13:17:41 +0900

    S09P12A806-312/feat: refactor naming createAuthToken() into createRefreshToken() in TokenProvider class
    Former-commit-id: 0bf759816ec05a9d2cd46ee23b1a0229956ca76f

--- be/src/main/java/com/wehee/domain/auth/token/AuthToken.java
+++ be/src/main/java/com/wehee/domain/auth/token/AuthToken.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03449d44ffd1a31ab4b4c1b35ab9ff97a7ee4bfad82960cc032ad1537148c39b
-size 2850
+oid sha256:1f70e935472402f08fd71a0f38b458530c4e20fbfef43871c24ea8719506d8a1
+size 2820

6ce91598ba5855a1bd891a36c33facdf5089c69d
Yejining <kimyejin.kr@gmail.com>
2023-08-11 16:11:40 +0900

    feat: new conroller for my chat room service
    Former-commit-id: 54ae57e6df54a535a4c755446b790ccc77748916

---
+++ be/src/main/java/com/wehee/api/chat/controller/MyChatRoomController.java
@@ -0,0 +1,12 @@
+package com.wehee.api.chat.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/mychat")
+@RequiredArgsConstructor
+public class MyChatRoomController {
+
+}

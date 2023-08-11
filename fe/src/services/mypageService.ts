347745570bee087c543e276b86a28cfcf930d650
ryunayeon <yoonayeon@gmail.com>
2023-08-11 11:12:38 +0900

    S09P12A806-287/feat: checkUpdateAvailability 추가
    Former-commit-id: 8e0af214ec74c79f4dbbacd16aafb92b5ce53993

--- fe/src/services/mypageService.ts
+++ fe/src/services/mypageService.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79a630f75c1f00ab5ddad73b125e8f311833989ce16594fb79f7f4270cd6e2c2
-size 1267
+oid sha256:495e2d6c68e6053e4e542d59d4fbda03a1260bd058912da91738c935639ec490
+size 1419

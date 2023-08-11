4eb491c0bfcbdf152a5ba470ca7435e37d8fac79
Yejining <kimyejin.kr@gmail.com>
2023-08-11 16:10:43 +0900

    S09P12A806-313/feat: find chatrooms for love on chat page
    Former-commit-id: 7b65b35cea4203ed82cd2c814084e68c9e1c096f

--- be/src/main/java/com/wehee/domain/chat/entity/ChatRoom.java
+++ be/src/main/java/com/wehee/domain/chat/entity/ChatRoom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42838dd401ece8b5d325b40fd43c81285f8c9289b1e2cc72b567545e3456806d
-size 2464
+oid sha256:451e69c174608e1f00374bb2d7f81aeba36b1cfea1150cf749aaed36f44f31ed
+size 2497
--- be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
+++ be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
@@ -1,9 +1,13 @@
 package com.wehee.domain.chat.service;
 
+import com.wehee.api.chat.dto.ChatRoomResponseDto;
+import com.wehee.api.chat.dto.ChatRoomSearchCondition;
 import com.wehee.domain.chat.entity.ChatRoom;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ChatRoomService {
     ChatRoom save(ChatRoom chatRoom);
+    List<ChatRoomResponseDto> findByCategoriesAndKeyword(ChatRoomSearchCondition condition);
 }

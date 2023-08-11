b275ca32220ac6d6e4188cb622b3affd5d8b6f70
yeonuy <jyeonuy@gmail.com>
2023-08-11 10:15:48 +0900

    S09P12A806-227/feat: 마이페이지 수정 모달 완성 최종
    Former-commit-id: 6c6763d33730c88770a62839667404cb0b10de38

--- fe/src/hooks/useSocialLogin.ts
+++ fe/src/hooks/useSocialLogin.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0d4a75411f9f41c5e970750df4fba787bba72875ebfa573d9ab7a7f2750d579
-size 2506
+oid sha256:28dab2e27d807bade2ae73194e04108e6ca9c6e8c323830fe06635a4ee988313
+size 2475

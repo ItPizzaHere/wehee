31ae6a1554adc1b5bd8c0b99ab0b01d7bb32ef9a
Yejining <kimyejin.kr@gmail.com>
2023-08-12 17:07:03 +0900

    S09P12A806-315/feat: check if user can create new chat room
    Former-commit-id: e1cf81ba557d9f82ebb72772aa9303749f89cb68

--- be/src/main/java/com/wehee/api/chat/controller/LoveOnChatController.java
+++ be/src/main/java/com/wehee/api/chat/controller/LoveOnChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebf2f9fc3f956e78772e664704ced5a7d84713ed525bcc4314f4c47f7d50a425
-size 882
+oid sha256:1728889e61975da6b2ba0db040b49b7f5eb4f7621e2bc11d2843f64751178225
+size 1168
--- be/src/main/java/com/wehee/domain/user/entity/User.java
+++ be/src/main/java/com/wehee/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e34b5c505120ee2cb7326ddba2ed4651634a8bdcd389e0f5b92840d36e421bc5
-size 4200
+oid sha256:fca17928fd12d05b983fceb72616cc978788c9b081852be57a0d4ffad466e902
+size 4692

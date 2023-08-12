7d62ef99793e053cba48f982cabef5fc1267ab3d
yeonuy <jyeonuy@gmail.com>
2023-08-13 07:38:45 +0900

    S09P12A806-293/feat: 게시글에 수정/삭제 버튼 추가
    Former-commit-id: 2422528bb22f156e4ea32ebd13a03bf1bef1367d

--- fe/src/components/board/ViewPost.tsx
+++ fe/src/components/board/ViewPost.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12815149ebcb6dc319b97b086188224621d739e43560cc68022bd9f79b379dfe
-size 2475
+oid sha256:30832e93a2a8620482ffbe65f0b2a05063f99548def7a988963eb2eb1b08eb67
+size 3304

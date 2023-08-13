e4951dc8fd15fcbba99b533e6eda6231e4e68b9d
박우현 <parkstarboard@gmail.com>
2023-08-14 00:25:05 +0900

    S09P12A806-291/feat: add column for moving table about deleted post&comment
    Former-commit-id: 2a223ee5a03dac2ac650f6c9aea2bac6d29ac46d

--- be/src/main/java/com/wehee/domain/lounge/service/LoungeService.java
+++ be/src/main/java/com/wehee/domain/lounge/service/LoungeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63795358945524528358b446e396d5c99202f602d5f34a3ace17aadd25aa100d
-size 24170
+oid sha256:25394c56a4d88f723fdce94c390f0172ee69b0aaabf8a4fb583456e9e39247ae
+size 24342

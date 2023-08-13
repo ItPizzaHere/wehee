ea754661fc85c737066a6517a406a92816e1129d
Yejining <kimyejin.kr@gmail.com>
2023-08-13 17:02:57 +0900

    feat: rename createUser() to create()
    Former-commit-id: ea9a2d18dfc5d7addff476843049cd6e1f2ab2a1

--- be/src/main/java/com/wehee/domain/user/entity/User.java
+++ be/src/main/java/com/wehee/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fca17928fd12d05b983fceb72616cc978788c9b081852be57a0d4ffad466e902
-size 4692
+oid sha256:efdeff50bda1ba3f6fb8c7f9df48e8acfa4393673c06f2be026ccbe0b1a4d5ff
+size 4688

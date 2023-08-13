86b0c21e72ed488d595a3b81eca004c77f0dcb77
박우현 <parkstarboard@gmail.com>
2023-08-14 01:49:58 +0900

    S09P12A806-291/feature: implement hotpost list, hotpost view
    TODO: ADD MySQL scheduler
    delimiter |
    create event if not exists moveTable
    on schedule every 12 hour
    starts '2023-08-14 00:00:00'
    on completion not preserve
    do
        begin
            insert into deleted_comment(comment_id, comment_post_id, comment_user_id, content, upload_time, state, deleted_time)
            select comment_id, comment_post_id, comment_user_id, content, upload_time, state, deleted_time from comment where state != 0 and deleted_time <= date_add(now(), interval -1 day);
            delete from comment where state != 0 and deleted_time <= date_add(now(), interval -1 day);
            insert into deleted_post(post_id, post_mbti_id, title, content, post_user_id, like_count, hit, comment_count, upload_time, state, deleted_time)
            select post_id, post_mbti_id, title, content, post_user_id, like_count, hit, comment_count, upload_time, state, deleted_time from post
            where state != 0 and deleted_time <= date_add(now(), interval -1 day);
            delete from post where state != 0 and deleted_time <= date_add(now(), interval -1 day);
            delete from hot_post;
    		insert into hot_post(`rank`, hot_post_id) select row_number() over(order by like_count desc, comment_count desc, hit desc, upload_time desc), post_id from post where state = 0 and upload_time>=date_add(now(), interval -1 day) order by like_count desc, comment_count desc, hit desc, upload_time desc LIMIT 16;
        end |
    delimiter ;
    
    Former-commit-id: 8b7febc7a00d0fba873d35514c076aa1f81e818f

--- be/src/main/java/com/wehee/domain/lounge/service/LoungeService.java
+++ be/src/main/java/com/wehee/domain/lounge/service/LoungeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25394c56a4d88f723fdce94c390f0172ee69b0aaabf8a4fb583456e9e39247ae
-size 24342
+oid sha256:d0725908c2a2b84b56fac070ca208e590598619cb712db38edd04da6407cb03f
+size 27165

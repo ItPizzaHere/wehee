263f0d400705081706e3523db52956c8da2e86d0
Yejining <kimyejin.kr@gmail.com>
2023-08-13 17:56:11 +0900

    S09P12A806-315/feat: create chat room
    Former-commit-id: 46069d2454da6d5be1f984419851c313dcd2e39e

--- be/src/main/java/com/wehee/api/chat/controller/LoveOnChatController.java
+++ be/src/main/java/com/wehee/api/chat/controller/LoveOnChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1728889e61975da6b2ba0db040b49b7f5eb4f7621e2bc11d2843f64751178225
-size 1168
+oid sha256:4902f2f991690b04358d240d0b33fb44eaf3cc82647bec04d4a7b67ffe753191
+size 2233
--- be/src/main/java/com/wehee/domain/chat/entity/ChatRoom.java
+++ be/src/main/java/com/wehee/domain/chat/entity/ChatRoom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:577024d9c19d9fe2fc95ac864abb1df8e255014e8b010c894c2321df4a319c79
-size 1309
+oid sha256:1b3d9a2d9c0f63ef3abc4b7f8af1a4b3cdc185ebac69069590815e64bd68d34f
+size 2807

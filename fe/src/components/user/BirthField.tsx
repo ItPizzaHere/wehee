afd6eb3a88f85c2234501564eca7630022003da4
ryunayeon <yoonayeon@gmail.com>
2023-08-14 10:49:33 +0900

    S09P12A806-321/fix:birth타입 변경
    Former-commit-id: 772aedc9dff9daef4eb896df8decc271750214cb

--- fe/src/components/user/BirthField.tsx
+++ fe/src/components/user/BirthField.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:832b64fe7aca8d040a3fd482e42f2cdaa28615eb1c5125be66189d6de5b789f4
-size 1361
+oid sha256:6e38d57b2e0548be01a88436ae04538d34376e8c6f9314cabf4f9c4406330806
+size 1369
--- fe/src/pages/UserAddInfo.tsx
+++ fe/src/pages/UserAddInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9facd2644e223565bf81799f5d6fb0bd0f406f0dcdee84a4ac82c5b7b322c51f
-size 4995
+oid sha256:061179bdacead29e793d2e6e246a41cca59c8812185033d8b84aeceb6cd3f154
+size 4997

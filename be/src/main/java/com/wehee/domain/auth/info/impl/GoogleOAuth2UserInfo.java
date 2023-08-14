1add53868bb3649c08e86f1ea62948ff4e21567b
ryunayeon <yoonayeon@gmail.com>
2023-08-14 10:24:26 +0900

    S09P12A806-321/fix:birth타입을 string으로
    Former-commit-id: b2b011da4d8b49b6f1f395fea538187eabac7745

--- be/src/main/java/com/wehee/domain/auth/info/impl/GoogleOAuth2UserInfo.java
+++ be/src/main/java/com/wehee/domain/auth/info/impl/GoogleOAuth2UserInfo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88b3c05133d68a979836b283010a5212d6bba46b0365a5f5e5726bb95cab4562
-size 1184
+oid sha256:96dc4f843f08097f2ad3c2f049c749e4134c6fd7adbaa3ef8f28b3a43c79e61c
+size 1202

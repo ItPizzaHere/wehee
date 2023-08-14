0c58db463867b1fbc18f9da9dcb8d51f7924dc71
Yejining <kimyejin.kr@gmail.com>
2023-08-14 13:31:13 +0900

    fix: fix user birth type occured error
    Former-commit-id: e79e8fb14eb5669d2eed2afc5f666bddbfbc73c0

--- be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
+++ be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fa7d65ce06aa7f686cca8703f2cedf6e17e62b88d967fdf9590afaeda137ce2
-size 6672
+oid sha256:dc0b3d4afe9d6641207b9062d854f055ee537d0f57fd1aec86bc7c98859ea314
+size 6744

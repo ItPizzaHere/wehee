ba8550dc9f9bdf3dc102ee3e411c2a4cb5acccff
Yejining <kimyejin.kr@gmail.com>
2023-08-14 16:58:01 +0900

    S09P12A806-348/feat: update user profiles when new user joins chatting room
    Former-commit-id: f4a381af40bb58e3b8f49be8d1d4fea9c4f37026

--- be/src/main/java/com/wehee/api/chat/controller/ChatRoomController.java
+++ be/src/main/java/com/wehee/api/chat/controller/ChatRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cacd69f6decfb36b00933d4d6fd3bbc76583680c10dee31a12ae74fdea4968cd
-size 3316
+oid sha256:53bde5a6002db682561873202b8e1a3c14094913bf9d64f74995db1d09527b05
+size 3509

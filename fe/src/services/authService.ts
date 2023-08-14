bf49b867cfc3f6b2a67d710d93d0fd9fa7a7391c
ryunayeon <yoonayeon@gmail.com>
2023-08-14 10:49:33 +0900

    S09P12A806-321/fix:birth타입 변경
    Former-commit-id: 080e8605c817de246eef7247dec8f8b92d07c389

--- fe/src/services/authService.ts
+++ fe/src/services/authService.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ed14b50b39037ecc3665a0b94247da098451f6dd0ce719db8fa3fb58d364359
+oid sha256:663b0f9bf8bcd453420e40cda2fcfee83863b1c2c7ce1421faee6487725e4d82
 size 1074

0cf10d749509e3a8da99ee6f4095b1a277f4d793
Yejining <kimyejin.kr@gmail.com>
2023-08-15 08:48:36 +0900

    S09P12A806-348/feat: leave chat room
    Former-commit-id: 93840a201bf6274c36c27c8843eb9659054dd73e

--- be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
+++ be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7cbce349790f0eeef1ce66a292a0d4a6bc4781058edeef11fa7d336595174bb
-size 7793
+oid sha256:63aeabf9b2b0070343b31f3328531f7f5380d3fb102c3a8f946656d8411bab50
+size 7846

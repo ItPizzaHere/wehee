707c9fed8a3967ed0924c381b5443cfdd3bf7c0c
sjyoon <sjyoon4755@gmail.com>
2023-08-15 22:10:38 +0900

    feat: voice-entity
    Former-commit-id: 8fcaf02be366493e4be4d42e1f296e4af118660e

--- be/src/main/java/com/wehee/domain/voice/controller/VoiceController.java
+++ be/src/main/java/com/wehee/domain/voice/controller/VoiceController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44b354c9658039c8b34512d720af211c13957c1206f10d123b0e768e0a2607c4
-size 2572
+oid sha256:0e85b7a026a7fa9c1ecda713c80d0868217aa9fcbf9be354598928bc62b2820e
+size 2579

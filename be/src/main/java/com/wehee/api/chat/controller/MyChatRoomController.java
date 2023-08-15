388cd9783ac0beaa302c5b04e7d5ef227ad35b41
Yejining <kimyejin.kr@gmail.com>
2023-08-15 10:11:33 +0900

    S09P12A806-200/feat: get my chatting room
    Former-commit-id: d696bca6c0aea80e7bf58e0e685babd07529e318

--- be/src/main/java/com/wehee/api/chat/controller/MyChatRoomController.java
+++ be/src/main/java/com/wehee/api/chat/controller/MyChatRoomController.java
@@ -1,12 +1,3 @@
-package com.wehee.api.chat.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("api/v1/mychat")
-@RequiredArgsConstructor
-public class MyChatRoomController {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:de51131b7a87309636c4f35836028548d8c650ff76c500ea2afc0d8e5bf0145a
+size 1168
--- be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
+++ be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63aeabf9b2b0070343b31f3328531f7f5380d3fb102c3a8f946656d8411bab50
-size 7846
+oid sha256:249f9f3f6cf420de4910430b8d1787c05e4b44172c6c05edbf7b1ec3acf4b066
+size 8550

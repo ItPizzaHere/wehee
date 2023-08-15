8686733eaf6b619fdd5e94a9b04e2e77e6930f40
sjyoon <sjyoon4755@gmail.com>
2023-08-15 22:10:38 +0900

    feat: voice-entity
    Former-commit-id: 755dd38781c316696917e0e9590a0ec7ef5e2817

--- be/src/main/java/com/wehee/domain/voice/Voice.java
+++
@@ -1,4 +0,0 @@
-package com.wehee.domain.voice;
-
-public class Voice {
-}
---
+++ be/src/main/java/com/wehee/domain/voice/entity/VoiceRoom.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5855235250142b790af03c7004ab28d550a970a2f34a9699ad94cbbd27bf22ab
+size 1235

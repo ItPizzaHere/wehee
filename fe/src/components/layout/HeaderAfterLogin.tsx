6de2ca2122a5e874aeb3c0574c3eb15b44f5c73f
yeonuy <jyeonuy@gmail.com>
2023-08-16 04:33:05 +0900

    S09P12A806-325/fix: 헤더에서 채팅 뱃지 삭제(주석처리함)
    Former-commit-id: 3ecd898c0942c833681753c3c20144c7c07968af

--- fe/src/components/layout/HeaderAfterLogin.tsx
+++ fe/src/components/layout/HeaderAfterLogin.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74ce7c6038464b34f18ec9c634ccdbdf479579488332f6d4bec7c98db109bd3e
-size 5880
+oid sha256:febe67e464fd48d31ab4005a14d7580fba4fb0bad79a5c5bb398c2557d97f501
+size 5896

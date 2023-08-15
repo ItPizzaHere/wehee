cb4eac70a148333a2002e722d491efb42035a43c
yeonuy <jyeonuy@gmail.com>
2023-08-15 21:47:54 +0900

    S09P12A806-325/feat: 채팅 관련 UI 마무리
    Former-commit-id: d79d3db99dd48bad99a1016ad4339559d4cc826a

--- fe/src/components/chat/Chatting.tsx
+++ fe/src/components/chat/Chatting.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:866f4a602aca429a08af744f00e2c855b2ed7e7f5cb7fdd8e46cabba701f0579
-size 1481
+oid sha256:a379f021db9fbdc53c348ef7c5641e394843072f130254e4c30ad2962e9ff0e7
+size 5396

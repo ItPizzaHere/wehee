cb8ef280c018c253dc9d0efe1e04f778346654d1
yeonuy <jyeonuy@gmail.com>
2023-08-16 14:41:12 +0900

    (S09P12A806-355/feat: love on chatlist 요청 (검색 및 일반, 카테고리는 x)
    Former-commit-id: dee2f4c5bb04fe3f194d226b2bf789ee6ef2d3d4

--- fe/src/components/chat/OnChatList.tsx
+++ fe/src/components/chat/OnChatList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fcceb2a52afaa11b4ec8550398bf9d15d2e39efadc72e96499a5fe3d69f3efa
-size 1784
+oid sha256:d51e95345a9c36311154d587a3956055c9062215c2091f77e0c0c8a0026ca252
+size 1613
---
+++ fe/src/services/onChatService.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee9547c8bc5d3204f32fba17495b51363f26203b4d022ce3d385f2fc33342f4b
+size 706

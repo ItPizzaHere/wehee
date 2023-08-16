fff8300a995fa86b10fa877db45047ace9eb7745
박우현 <parkstarboard@gmail.com>
2023-08-16 17:52:48 +0900

    S09P12A806-339/feat: listPost done, trying viewPost now
    Former-commit-id: 22859322e0091e33e3a348366c19981045c7a48e

--- fe/src/components/board/BoardHome.tsx
+++ fe/src/components/board/BoardHome.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a4924c926b20a52290496d1a46984eb1746bd48b25c4894048a60a57bf7db8b
-size 2268
+oid sha256:be9b0d90105d72fc01d21953fc89e2b5673ed8de702d4e2f00012dc34b2827ee
+size 2558
--- fe/src/redux/postSlice.ts
+++ fe/src/redux/postSlice.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5008354b731ce10766e5119fd8629d4fa695e7110ed4e93f3a4119a42f7110fe
-size 1388
+oid sha256:1b187337ae935cff3a0ab7492bf99a3030fbef3ee6e31de243045db80f403924
+size 1531

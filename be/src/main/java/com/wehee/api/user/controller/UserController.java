debb8e8d95c2cfbcc9e3d34c58c3ebcb93a8c1b2
Yejining <kimyejin.kr@gmail.com>
2023-08-16 11:10:57 +0900

    S09P12A806-353/fix: update user info error
    Former-commit-id: 8d1045baaed55191a25dfb88afdd954e34f56619

--- be/src/main/java/com/wehee/api/user/controller/UserController.java
+++ be/src/main/java/com/wehee/api/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bafaf5dc46cf77b4ce1c07ff646ec1525619d0ab93499ef2ef03b6985c96db2
-size 2544
+oid sha256:ef5540a31717805a02490d506a4a0e42f8b41a34792f34310ea6091ee7a5f492
+size 2532

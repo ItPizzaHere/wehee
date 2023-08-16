35d3d9796537371b62cb0096d81e9be68f8f418d
yeonuy <jyeonuy@gmail.com>
2023-08-16 12:37:32 +0900

    S09P12A806-354/fix/유저 관련 500 에러 fix
    Former-commit-id: 7e3d45c1d3755be6a20718be9235ada0e68a6ac1

--- be/src/main/java/com/wehee/domain/voice/controller/VoiceController.java
+++ be/src/main/java/com/wehee/domain/voice/controller/VoiceController.java
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:44b354c9658039c8b34512d720af211c13957c1206f10d123b0e768e0a2607c4
-size 2572
+package com.wehee.domain.voice.controller;
+
+public class VoiceController {
+
+}

434e7d8bc188e86c1ccd5f509ab4cbc34df41ed4
Yejining <kimyejin.kr@gmail.com>
2023-08-16 13:02:46 +0900

    S09P12S806-353/feat; get activated users
    Former-commit-id: 8ae98e9fb93ea35c45ca4f21db384e09b370b0a3

--- be/src/main/java/com/wehee/api/chat/controller/MyChatRoomController.java
+++ be/src/main/java/com/wehee/api/chat/controller/MyChatRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ee641bdc094c8e7b8f27dc2ef1288d85cccadb4fd24f7747a99eec0611f3857
-size 2342
+oid sha256:370821045a08eb6f99e1a11151ff9bb041a0436c02ae61fbf0b711371987b883
+size 2917

d0f508899175b58996e906f01ad5e1df850e98c2
Yejining <kimyejin.kr@gmail.com>
2023-08-17 20:52:28 +0900

    S09P12A806-359/feat: send system message when user creates new chat room
    Former-commit-id: 5f9150691bfb50f6bfc75b235ee883e855980676

--- be/src/main/java/com/wehee/api/chat/controller/MyChatRoomController.java
+++ be/src/main/java/com/wehee/api/chat/controller/MyChatRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f22872bfaa7b502727cb428089ad8cbfcd7cecfed70488a474aca70a411b863c
-size 3033
+oid sha256:4a6c407b567a812592c1c1434ea6382ebc4475cdb78135482d086615fa9f0d17
+size 2911
--- be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
+++ be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02cb247550eab437bb587f9f875c16f54c44048a949b839965ec244f76eaf54e
-size 9877
+oid sha256:adc4a982eac057e5fcc1a6aefdce577b458cb974cf3491d925d572a329ea4d7f
+size 9922

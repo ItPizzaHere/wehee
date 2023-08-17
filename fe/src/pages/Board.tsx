94e6130a1d23a30955119b5d5c5c0d9e393c2d63
yeonuy <jyeonuy@gmail.com>
2023-08-17 17:51:34 +0900

    S09P12A806-357/fix: 메뉴 및 헤더에 현재 페이지 표시 안됨 이슈 해결
    Former-commit-id: 8ed95da2786fe288b8cf8e639438dfb9a33089eb

--- fe/src/pages/Board.tsx
+++ fe/src/pages/Board.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91519df5be08ab72ea34ccefdda3d164876a15f6893b59de874291604011bf94
-size 2684
+oid sha256:62de25e99febfce47e93039d8693ad06bf7d87c357aa878a73681b57b3b9f3be
+size 3146

a4f10725cf0c4906b9e8fd579f3e2291fb0afa3d
sjyoon <sjyoon4755@gmail.com>
2023-08-17 23:37:55 +0900

    sjy 2337
    Former-commit-id: b593b676ae9417cedadc002abb01051eab9db75f

--- be/src/main/java/com/wehee/api/voice/controller/VoiceRoomController.java
+++ be/src/main/java/com/wehee/api/voice/controller/VoiceRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e1aeba63a97e7512623b206d7530b524374bc69e9d90394e95d2fa6a8cfd32d
-size 5057
+oid sha256:9a46517199dfd1e8c50939c7dabdcbe84e07746dcb809352d6869f142c4b585e
+size 5157

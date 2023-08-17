fbc28ff9d18efc79c1b7054837e6100961c4ab80
Yejining <kimyejin.kr@gmail.com>
2023-08-18 01:48:31 +0900

    S09P12A806-363/feat: send messages and chatting room information
    Former-commit-id: 758a214d304a438b26ecc1f9af935d2ff764144d

--- be/src/main/java/com/wehee/api/chat/dto/MyChatRoomResponseDto.java
+++ be/src/main/java/com/wehee/api/chat/dto/MyChatRoomResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:accc08ffdf84aac36081767a1c46d789bc772007f9b5483d997c6896e1ffc9a8
-size 867
+oid sha256:77d760936631579cb6a0a525f0e70a1d32e3a30ba3ad08d8a9b08740fb3fdbbf
+size 1037

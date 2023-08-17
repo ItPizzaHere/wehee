e1354deb9437f642396bd40bc97ec31928d5d3c5
sjyoon <sjyoon4755@gmail.com>
2023-08-18 08:33:07 +0900

    fix
    Former-commit-id: dd852021966c71d639d7e1520911ae6b9cd1d20d

--- be/src/main/java/com/wehee/domain/voice/service/VoiceRoomService.java
+++ be/src/main/java/com/wehee/domain/voice/service/VoiceRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53bdaf0ba456d436786aba0020c6f28d2953517ee3453cbf63d8dde3b86a2460
-size 5180
+oid sha256:a4d2bdb86aba3ad9a780f076cd2ac08ff4f477784f28b764ffaaead9be2036ed
+size 5176

840ba170d382e1fac1c36d3351514d4990e773ae
Yejining <kimyejin.kr@gmail.com>
2023-08-17 15:11:41 +0900

    S09P12A806-350/feat: join chat room -> join or activate chat room
    Former-commit-id: dae4f98eed966d8002c9e47fb6c503b7dde247b5

--- be/src/main/java/com/wehee/api/chat/controller/ChatRoomController.java
+++ be/src/main/java/com/wehee/api/chat/controller/ChatRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dc3b5e4a14d5af84ad6ce6bd60ea391ae3fe1aba2bfc76f94677a1135904fb0
-size 3742
+oid sha256:1905d506ca0a1774cea171cdb7e33faffa4313a920aecf7ce4111a3582dd7984
+size 3752

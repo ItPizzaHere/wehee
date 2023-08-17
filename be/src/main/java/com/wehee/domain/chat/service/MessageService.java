6f04aac13b0e31d69c09ec81a9505edb4e39ed93
Yejining <kimyejin.kr@gmail.com>
2023-08-17 22:50:41 +0900

    S09P12A806-360/feat: fix redis add error
    Former-commit-id: cbd6a4dba117020ca648788c2084f72974a0db35

--- be/src/main/java/com/wehee/domain/chat/service/MessageService.java
+++ be/src/main/java/com/wehee/domain/chat/service/MessageService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b941da7b44d0bafda059ad00edc1482f3384e5fecfae2462d72e231f8c18f87
-size 2838
+oid sha256:840adea244d79fe5fdce346054260c0834578d4a7da07e93c32a144f6c01c7e9
+size 2842

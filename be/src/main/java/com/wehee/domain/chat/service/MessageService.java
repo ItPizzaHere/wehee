16182de179c54e5490aa648e6f2e900f3c978639
Yejining <kimyejin.kr@gmail.com>
2023-08-17 21:22:27 +0900

    S09P12A806-360/feat: fix websocket problem
    Former-commit-id: b5072fb5b4f89bd9e783dc6d80975b6422d1ad30

--- be/src/main/java/com/wehee/domain/chat/service/MessageService.java
+++ be/src/main/java/com/wehee/domain/chat/service/MessageService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b56825516dae974fcacdf5711e94f1b206b8c18b429ded7da6c885d3e0a7c754
-size 2832
+oid sha256:4b941da7b44d0bafda059ad00edc1482f3384e5fecfae2462d72e231f8c18f87
+size 2838

4bc8f5b1a8858915e4da2efba6581e12ea391d80
yeonuy <jyeonuy@gmail.com>
2023-08-17 10:04:22 +0900

    S09P12A806-355/feat: 23-08-17 10:00 작업내역
    Former-commit-id: f3eab741bbcfd4c4cc714cb16c33671aed0e3f4b

--- fe/src/components/chat/ModalCreateChat.tsx
+++ fe/src/components/chat/ModalCreateChat.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2635effc5cafca5235da88864e4ec534bf8124a59bc29db12f0f82ffb295075e
-size 6227
+oid sha256:40a96837a93500733ced6f36cc3d2c7f8cf272caf86634676ebf0982ecbd5869
+size 6213

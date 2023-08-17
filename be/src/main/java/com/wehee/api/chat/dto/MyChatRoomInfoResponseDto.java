f407ff360e340de8147edcc821f2458e40b3b8cf
Yejining <kimyejin.kr@gmail.com>
2023-08-17 17:42:20 +0900

    S09P12A806-358/feat: get last message when listing my chatting list
    Former-commit-id: 6eceb89346538ab2055b5c6373714025c7cf03ec

--- be/src/main/java/com/wehee/api/chat/dto/MyChatRoomInfoResponseDto.java
+++ be/src/main/java/com/wehee/api/chat/dto/MyChatRoomInfoResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e7a040f7d36133543c21c10b448be56086402c36d00ed60dbc076ebb678501d
-size 1123
+oid sha256:bd6835fcfe0d2128549040d4225948aa2faacb18dfd38b6d77975f9fce4cdd3d
+size 1194
--- be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
+++ be/src/main/java/com/wehee/domain/chat/service/ChatRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dffe09b7412596289d5e9f0fe6ff165be9f330e16817f1bdb01e8c2c84c3ace
-size 9048
+oid sha256:02cb247550eab437bb587f9f875c16f54c44048a949b839965ec244f76eaf54e
+size 9877

64f142de64e9b9cb1d15a17c63c8c33f57a44aa4
yeonuy <jyeonuy@gmail.com>
2023-08-18 04:36:55 +0900

    dd
    Former-commit-id: 1a1daeb812544398cbe6841f8fdd9903629c7922

--- be/src/main/java/com/wehee/domain/chat/repository/MessageRepository.java
+++ be/src/main/java/com/wehee/domain/chat/repository/MessageRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:358ff0e441c2ff84e0695b128dfa22964766c3f4ae9b64a68cf71001db557f63
-size 534
+oid sha256:de298ee5b859eafb8e9255a1db6c4516eff0e5bff70f36ae2431e1d0ae768d00
+size 536

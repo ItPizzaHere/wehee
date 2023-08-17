a7edfb3732c90f815f8c873ab86b1f2b324af310
Yejining <kimyejin.kr@gmail.com>
2023-08-17 14:05:04 +0900

    S09P12A806-350/feat: create chat room (be/fe)
    Former-commit-id: 87b85fb1d70201b526e5fcbe21c5951621798908

--- be/src/main/java/com/wehee/domain/chat/entity/ChatRoom.java
+++ be/src/main/java/com/wehee/domain/chat/entity/ChatRoom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f460e1cd6f703edfe1dc8bd042502ea8128b063426be1643e6e87a5cf80801cb
-size 5270
+oid sha256:b4639dc23e6f8303d5ddece76440353da73bc27d6a7f40ce88abab125a20357c
+size 5304

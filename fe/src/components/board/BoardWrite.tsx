2f4618d687c6379a51d659f1cc313c2280fe3502
박우현 <parkstarboard@gmail.com>
2023-08-17 10:04:45 +0900

    S09P12A806-339/fix: fix error(listing) after writing a post
    Former-commit-id: bd7b2d53307c12c845a1380c52c627a68cca7885

--- fe/src/components/board/BoardWrite.tsx
+++ fe/src/components/board/BoardWrite.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:780b59467ac49c3060a2194cb54d1b65a49f99b88da2e9e42e8eb98c76a9ebe2
-size 2919
+oid sha256:12f3053b6ab15127c48d954175b4cf39e94478eac03d66949af69cea6a286bee
+size 2949

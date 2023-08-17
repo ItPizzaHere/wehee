19f3b8e534070f7b6d08f4d0babec6923ab68f78
Yejining <kimyejin.kr@gmail.com>
2023-08-17 13:28:53 +0900

    S09P12A806-350/fix: isJoinedLessThen() error in User.java
    Former-commit-id: 9727f2520e7658269fc741d4b257fd002bc5bbed

--- be/src/main/java/com/wehee/domain/user/entity/User.java
+++ be/src/main/java/com/wehee/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e564dab7c4822bd8c822282b7b3cfb328e83367e0634aeed86d02319271c5a33
-size 4773
+oid sha256:3c29ed9e52c7ef3691387da968ba87a68273d3aed1415ad501a78b5f8ebbbe0f
+size 4774
